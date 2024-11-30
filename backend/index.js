cbff73a622d250c8a4a97545a7304837ec3e5a47
azrilfahmiardi <anakijummira@gmail.com>
2024-11-30 11:50:39 +0700

    auth callback eror

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: `${process.env.API_URL}/auth/google/callback`
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const username = profile.displayName;
