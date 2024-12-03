b6d1700e731ff0d2eff4911a602fe310394b7027
Bayu Maulana <77946987+bayuzzs@users.noreply.github.com>
2024-12-03 22:17:16 +0700

    Edit carousel and Table in detail campus page

--- frontend/src/pages/DetailKampus.jsx
+++ frontend/src/pages/DetailKampus.jsx
@@ -150,7 +150,7 @@ const DetailKampus = () => {
           </p>
         </div>
         <div className="flex flex-col-reverse items-center justify-center gap-10 px-5 pb-10 sm:px-20 md:flex-row">
-          <table className="grow table-fixed">
+          <table className="max-w-sm grow table-fixed sm:max-w-md md:max-w-lg lg:max-w-xl">
             <tbody>
               <tr className="gap-3 border-b border-t border-white">
                 <td className="w-[25%] px-2 py-2 font-bold text-white">
