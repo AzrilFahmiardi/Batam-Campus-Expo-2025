4cd242afb1eeb8267e4bf71c85706b642cb5815e
Bibiing <nabil.julian04@gmail.com>
2024-12-26 21:14:46 +0700

    add hero kegiatan

--- frontend/src/pages/Kegiatan.jsx
+++ frontend/src/pages/Kegiatan.jsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Calendar from "../components/Kegiatan/Calendar";
 import Cloud from "../components/Kegiatan/BackgroundClouds";
 import Footer from "../components/Footer";
+import Hero from "../assets/images/Kegiatan/hero.png";
 
 const Kegiatan = () => {
   const { user, isLoggedIn, hasVoted } = useAuth();
@@ -12,9 +13,7 @@ const Kegiatan = () => {
       <Header user={user} />
       <div className="relative bg-blue-gradient p-10">
         <Cloud />
-        <h1 className="mt-28 text-center font-pixelify text-4xl font-bold text-white sm:text-5xl md:text-6xl lg:text-7xl">
-          Kalender Kegiatan
-        </h1>
+        <img src={Hero} className="mx-auto mt-28 block w-full md:w-3/4" />
         <Calendar />
       </div>
       <div className="relative bg-footer-gradient">
