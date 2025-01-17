f3a92447a1e20ad583c0c6dcf73f304a2427f1fe
azrilfahmiardi <anakijummira@gmail.com>
2025-01-17 09:58:47 +0700

    edit cors

--- backend/index.js
+++ backend/index.js
@@ -83,7 +83,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: APP_URL,
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 
